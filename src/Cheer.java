fd523a70e8068da5553bd3a6d2bb6f98c8f5c21d
daniviki <daningerviki@gmail.com>
2019-04-02 14:47:24 +0200

    add wtf

--- src/Cheer.java
+++ src/Cheer.java
@@ -2,5 +2,6 @@ public class Cheer {
     public static void cheering(String name, String email){
         System.out.println("name: " + name + " & email: " + email);
         System.out.println("hungarian");
+        System.out.println("wtf");
     }
 }
