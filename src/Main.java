06e6817360f5b395eec035857bce3ad48f1887d3
Karoly Valentin <valentin.karoly@gmail.com>
2019-04-02 12:15:55 +0200

    b1

--- src/Main.java
+++ src/Main.java
@@ -7,4 +7,8 @@ public class Main {
     public static void greet(String name){
         System.out.println("Hello " + name + "! How is your day going so far?");
     }
+
+    private static void count() {
+        System.out.println(28);
+    }
 }
