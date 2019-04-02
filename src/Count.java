ab712de1b7d70b90aec0082b843a5351f3a7303d
Karoly Valentin <valentin.karoly@gmail.com>
2019-04-02 13:00:39 +0200

    b5

--- src/Count.java
+++ src/Count.java
@@ -1,7 +1,8 @@
 public class Count {
 
-    private static void count(int year) {
+    private static void count(int year, int a) {
         System.out.println(year);
+        System.out.println(a);
     }
 
 }
