e943c66a037782f696101167afbc5acdde07426e
Tamas Puskas <tamasp1993@live.com>
2019-04-02 12:00:59 +0200

    Added greet function

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       greet("Tamás");
+    }
+
+    public static void greet(String name){
+        System.out.println("Hello " + name + "! How is your day going so far?");
     }
 }
